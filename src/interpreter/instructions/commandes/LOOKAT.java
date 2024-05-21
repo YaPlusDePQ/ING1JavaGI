caac5f74d3ab9147bfcf34a36d74d3c9862c6376
Epsi574 <pillotpier@cy-tech.fr>
2024-05-21 16:37:32 +0200

    Enfin LOOKAT

--- src/interpreter/instructions/commandes/LOOKAT.java
+++ src/interpreter/instructions/commandes/LOOKAT.java
@@ -4,81 +4,106 @@ import java.util.List;
 
 import fx.DrawingCursor;
 import fx.DrawingTab;
+import interpreter.Parser;
+import interpreter.Exceptions.InvalidArgument;
+import interpreter.Exceptions.SyntaxError;
 import interpreter.variables.Variable;
 import interpreter.variables.VariableNumber;
 import interpreter.variables.VariableString;
-import javafx.scene.paint.Color;
 
 /*
  * 
  * Ya qu'une seule commande look at donc tu dois dasn cette classe determiner quoi faire en fonction des argumetn envoyé (exemple dans COLOR)
  */
-public class LOOKAT extends Command{
+public class LOOKAT implements Command{
     
-    public static void execute(DrawingTab tab, List<Variable> args) throws IncorrectArgument{
+        public void execute(DrawingTab tab, List<Variable> args) throws SyntaxError,InvalidArgument{
+       
+            double valueX = 0;
+            double valueY = 0;
+            double tempX = 0;
+            double tempY = 0;
+            double modul = 0;
+            double angle = 0;
+            int cursorId = 0;
+        
+            List<DrawingCursor> cursors = tab.getAllDrawingCursor();
+        
+            //For the case of x, y
+            if(args.size() == 2){
+            
+                if(args.get(0) instanceof VariableNumber && args.get(1) instanceof VariableNumber){ 
+                    valueX = ((VariableNumber)args.get(0)).getValue(); 
+                }
+                else if(args.get(0) instanceof VariableString){ 
+
+                if( !((VariableString)args.get(0)).getValue().matches("([0-9]*\\.?[0-9]*) *%") ){ // check if its a %, if not throw an error
+                    throw new InvalidArgument("Arguments must be pourcentages [String] or numbers [Integer/Double]");
+                }
+
+                valueX = Parser.percentageToDouble((String)args.get(0).getValue()); //convert the value from a string to a number
+                valueX = tab.getWidth() > tab.getHeight() ? (valueX)*tab.getWidth() : (valueX)* tab.getHeight(); // it as to be a pourcentage of the biggest value between widht and height
+        
+                }
+                else{
+                    throw new InvalidArgument("Arguments must be pourcentages [String] or numbers [Integer/Double]");
+                }
+        
+                if(args.get(1) instanceof VariableNumber){ 
+                    valueY = ((VariableNumber)args.get(1)).getValue();
+                }
+                else if(args.get(1) instanceof VariableString){ 
+
+                    if( !((VariableString)args.get(1)).getValue().matches("([0-9]*\\.?[0-9]*) *%") ){ // check if its a %, if not throw an error
+                        throw new InvalidArgument("Arguments must be pourcentages [String] or numbers [Integer/Double]");
+                    }
+
+                    valueY = Parser.percentageToDouble((String)args.get(1).getValue()); //convert the value from a string to a number
+                    valueY = tab.getWidth() > tab.getHeight() ? (valueY)*tab.getWidth() : (valueY)* tab.getHeight(); // it has to be a pourcentage of the biggest value between widht and height
+
+                }
+                else{
+                    throw new InvalidArgument("Arguments must be pourcentages [String] or numbers [Integer/Double]");
+                }
+            }
+
+            else if(args.size() == 1){
+            
+            //check type
+                if (args.get(0) instanceof VariableNumber){
+                    //getting the value
+                    cursorId = ((VariableNumber)args.get(0)).getValueInt();  
+                    valueX = cursors.get(cursorId).getCurrentX();
+                    valueY = cursors.get(cursorId).getCurrentY();
+                } 
+
+                else{
+                    throw new InvalidArgument("Argument must be 1 number [Integer/Double]");
+                }
                 
-        double valueRed = 0;
-        double valueGreen = 0;
-        double valueBlue = 0;
-        
-        List<DrawingCursor> cursors = tab.getAllDrawingCursor();
-        
-        //For the case of x,x,x as arguments (RGB)
-        if(args.size() == 3){
-            
-            //check type
-            if( !(args.get(0) instanceof VariableNumber) || !(args.get(1) instanceof VariableNumber) || !(args.get(2) instanceof VariableNumber)){
-                throw new IncorrectArgument("parametre(s) incorrect(s)");
-            }
-
-
-            //getting values
-            valueRed = ((VariableNumber)(args.get(0))).getValue(); 
-            valueRed = valueRed*255;
-
-            valueGreen = ((VariableNumber)(args.get(1))).getValue();
-            valueGreen = valueGreen*255;
-
-            valueBlue = ((VariableNumber)(args.get(2))).getValue();
-            valueBlue = valueBlue*255;
-
-            //checking if the value are correct IE: RGB goes from 0- to 255
-            if(valueRed > 255 || valueRed < 0 || valueGreen > 255 || valueGreen < 0 || valueBlue > 255 || valueBlue < 0){
-                throw new IncorrectArgument("parametre(s) incorrect(s)");
-            }
-            
-            //apply the change
-            for(int i=0; i<cursors.size(); i++){
-                if(cursors.get(i).isActive()){
-                    cursors.get(i).setColor(Color.rgb((int)valueRed, (int)valueGreen, (int)valueBlue));
+                //apply the change
+                for(int i=0; i<cursors.size(); i++){
+                    if(cursors.get(i).isActive()){
+                        tempX = valueX-(cursors.get(i).getCurrentX()); //locating the position X,Y in a new coordinate system with the current cursor as the origin 
+                        tempY = valueY-(cursors.get(i).getCurrentY());  
+                        modul = Math.sqrt(Math.pow(tempX,2) + (Math.pow(tempY,2))); //calculating the modul of the X,Y position 
+                        if (tempY >= 0){ //tempX/modul still give two possible positions, we determine which one is correct with y value
+                            angle = ((Math.acos(tempX/modul))/Math.PI)*180;
+                        }
+                        else{
+                            angle = 180+((Math.acos(tempX/modul))/Math.PI)*180;
+                        cursors.get(i).setDirection(cursors.get(i).getDirection()+angle);
+                        }
+                    }
                 }
-            }
-
-        }
-        else if(args.size() == 1){ //For the case of #xxxxxx  as argument (HEX)
+            }        
+                        
+               
             
-            //check type
-            if( !(args.get(0) instanceof VariableString) ){
-                throw new IncorrectArgument("parametre(s) incorrect(s)");
-            }
 
-            //checking if the value are correct IE: being a hex
-            if( !( (VariableString)args.get(0) ).getValue().matches("#([a-fA-F0-9]{6})") ){
-                throw new IncorrectArgument("parametre(s) incorrect(s)");
-            }
-
-            //apply the change
-            for(int i=0; i<cursors.size(); i++){
-                if(cursors.get(i).isActive()){
-                    cursors.get(i).setColor(Color.web( ((VariableString)args.get(0)).getValue() ));
-                }
-            }
-
+        else {
+            throw new SyntaxError("Need 1 or 2 argument");
+            
         }
-        else{
-            throw new IncorrectArgument("parametre(s) incorrect(s)");
-        }
-
     }
 }
-
